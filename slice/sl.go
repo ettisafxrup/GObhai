ca00daf2644dd7552f3f45c88908c255a8b4e1e7
ettisafxrup <ettisafrup@gmail.com>
2025-08-06 12:49:55 +0600

    slicing

--- slice/sl.go
+++ slice/sl.go
@@ -2,25 +2,17 @@ package main
 
 import "fmt"
 
-func changeSlice(s []int) []int {
-	s[0] = 69
-	return s
-}
-
 func main() {
-	x := []int{1, 2, 3, 4, 5}
-	x = append(x, 6)
-	x = append(x, 7)
+	x := []int{}
+	x = append(x, 1)
+	x = append(x, 2)
+	x = append(x, 3)
 
-	y := x[4:] // 5, 6, 7
+	y := x
 
-	y[0] = 69
-	fmt.Println(y[0])
+	x = append(x, 4)
+	y = append(y, 5)
+
 	fmt.Println(x)
-
-	// a := changeSlice(y) // 69, 6, 7
-
-	// fmt.Println(x)
-	// fmt.Println(y)
-	// fmt.Println(a)
+	fmt.Println(y)
 }
